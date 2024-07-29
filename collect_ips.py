1dfb4f69f5dc5caa2abfc883357151f7c9b9a5fc
EzSync <wugkdy@gmail.com>
2024-07-29 16:58:43 +0800

    Update collect_ips.py

--- collect_ips.py
+++ collect_ips.py
@@ -5,7 +5,7 @@ import os
 
 # 目标URL列表
 urls = ['https://monitor.gacjie.cn/page/cloudflare/ipv4.html', 
-        'https://stock.hostmonit.com/CloudFlareYes',
+        'https://ip.164746.xyz',
         ]
 
 # 正则表达式用于匹配IP地址
@@ -27,7 +27,7 @@ with open('ip.txt', 'w') as file:
         # 根据网站的不同结构找到包含IP地址的元素
         if url == 'https://monitor.gacjie.cn/page/cloudflare/ipv4.html':
             elements = soup.find_all('tr')
-        elif url == 'https://stock.hostmonit.com/CloudFlareYes':
+        elif url == 'https://ip.164746.xyz':
             elements = soup.find_all('tr')
         else:
             elements = soup.find_all('li')
